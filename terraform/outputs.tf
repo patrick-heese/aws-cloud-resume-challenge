d513f86056acb9c72f564ecd88e508a9ba2e9047
Patrick Heese <pheese8@gmail.com>
2025-11-15 16:48:19 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,20 +1,19 @@
 output "site_bucket" {
-  value = aws_s3_bucket.site.id
+  description = "S3 bucket name for the website"
+  value       = aws_s3_bucket.site.id
 }
 
-output "cloudfront_domain" {
-  value = aws_cloudfront_distribution.site.domain_name
+output "cloudfront_distribution_id" {
+  description = "CloudFront distribution ID for the website"
+  value       = aws_cloudfront_distribution.site.id
 }
 
-output "api_base_url" {
-  value = aws_apigatewayv2_stage.default.invoke_url
+output "cloudfront_domain_name" {
+  description = "CloudFront distribution domain name (e.g., d123.cloudfront.net)"
+  value       = aws_cloudfront_distribution.site.domain_name
 }
 
 output "ssm_api_param" {
-  value = aws_ssm_parameter.api_base_url.name
-}
-
-output "acm_certificate_arn" {
-  value       = try(aws_acm_certificate_validation.validated[0].certificate_arn, null)
-  description = "Present only when enable_route53 = true"
+  description = "SSM Parameter name that stores the API base URL"
+  value       = aws_ssm_parameter.api.name
 }
